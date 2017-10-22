9715fb38e855f309dbbc31882321a10748136c5a
Rahul Bairathi <rbairath@uncc.edu>
2017-10-22 13:56:39 -0400

    Initial Commit

---
+++ src/com/pa3/LCS.java
@@ -0,0 +1,23 @@
+/*
+    Programming Assignment 3
+    Title: Longest Common Subsequence
+    Team Members: Justin Toler, Rahul Bairathi, Shubhra Mishra
+ */
+
+package com.pa3;
+
+public class LCS {
+    /**
+     * Returns longest common subsequence of input strings
+     * @param seq1 Input String Sequence 1
+     * @param seq2 Input String Sequence 2
+     * @return Longest Common Subsequence
+     */
+    private String getLCS(String seq1, String seq2) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
